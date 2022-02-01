6d8cae7724c7f1e1eb54e62fcbe806745ce2be27
Emerson Vergara <ee-web@hotmail.com>
2022-01-31 21:32:24 -0500

    Ejercicios crear el modulo de contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './components/contador/contador.component';
+import { ContadorModule } from './components/contador/contador.module';
 import { HeroesModule } from './components/heroes/heroes.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContadorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
